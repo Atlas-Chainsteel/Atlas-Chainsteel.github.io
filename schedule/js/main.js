debcc37125821c880b92f5f214a90ece462fd355
Atlas-Chainsteel <griffenfang@gmail.com>
2025-11-09 11:42:33 -0600

    UPDATED TIMEZONE SHENANIGANS

--- schedule/js/main.js
+++ schedule/js/main.js
@@ -1,5 +1,15 @@
 let schedule = document.getElementsByClassName("row");
-const startTime=new Date("2025-10-01T17:00:00-05:00");
+const startTime=new Date("2025-10-01T17:00:00-06:00");
+
+//DO NOT USE ELSEWHERE!!!!!!!
+const Sunday = new Date("2025-11-08T17:00:00-06:00");
+console.log
+
+for (var i=0; i < schedule.length; i++) {
+  Sunday.setDate(Sunday.getDate() + 1);
+  schedule[i].getElementsByClassName("day")[0].innerHTML = Sunday.toLocaleDateString("en-US", { weekday: "long" });
+}
+
 const time = startTime.toLocaleTimeString('en-US', {
     hour: 'numeric',
     minute: '2-digit',
@@ -8,8 +18,13 @@ const time = startTime.toLocaleTimeString('en-US', {
 
 document.getElementById("timeStamp").innerHTML = time;
 
-const current = new Date();
+var current = new Date();
+console.log(current);
+console.log(current.toLocaleString("en-US", {timeZone: 'America/Chicago'}));
+console.log(Date.parse(current.toLocaleString("en-US", {timeZone: 'America/Chicago'})));
 current=new Date(Date.parse(current.toLocaleString("en-US", {timeZone: 'America/Chicago'})));
+
+
 
 for (let i=0; i < schedule.length; i++) {
     if (i<current.getDay()) {
